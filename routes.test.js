1b296ecfb63ae1a9510146555dd99077afff9b48
nori <__NORI__@chaos-2.lan>
2021-02-21 12:32:59 -0800

    added all tests for GET requests

--- routes.test.js
+++ routes.test.js
@@ -51,3 +51,84 @@ beforeEach(async () => {
       expect(res.text).toContain(testCustomer.last_name);
     })
   })
+
+  describe("GET /:id", () => {
+    test("Retrieves a customer when a valid customer id variable is passed as a query parameter", async () => {
+      const res = await request(app).get(`/${testCustomer.id}`)
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toContain(testCustomer.first_name);
+      expect(res.text).toContain(testCustomer.last_name);
+      expect(res.text).toContain(testCustomer.phone);
+      expect(res.text).toContain(testCustomer.notes);
+      expect(res.text).toContain('Reservations');
+      expect(res.text).toContain('September 8th 2018, 12:30 pm');
+      expect(res.text).toContain('September 8th 2018, 1:30 am');
+    })
+    test("Retrieves a 404 status and an error message when an invalid customer id is passed as a query parameter", async () => {
+        const res = await request(app).get('/5')
+        expect(res.statusCode).toEqual(404);
+        expect(res.text).toContain('No such customer: 5');
+    })
+    test("Retrieves a 400 status and an error message when a non integer id is passed as a query parameter", async () => {
+        const res = await request(app).get('/blargh')
+        expect(res.statusCode).toEqual(400);
+        expect(res.text).toContain('Invalid customer id. Must be of type Integer');
+    })
+  })
+
+  describe("GET /:id/edit", () => {
+    test("Retrieves a form for editing customer information when a valid customer id is passed as a query parameter", async () => {
+      const res = await request(app).get(`/${testCustomer.id}/edit`)
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toContain('Edit Customer');
+      expect(res.text).toContain(testCustomer.first_name);
+      expect(res.text).toContain(testCustomer.last_name);
+      expect(res.text).toContain(testCustomer.phone);
+      expect(res.text).toContain(testCustomer.notes);
+      expect(res.text).toContain('Add');
+    })
+    test("Retrieves a 404 status and an error message when an invalid customer id is passed as a query parameter", async () => {
+        const res = await request(app).get('/999')
+        expect(res.statusCode).toEqual(404);
+        expect(res.text).toContain('No such customer: 999');
+    })
+    test("Retrieves a 400 status and an error message when a non integer id is passed as a query parameter", async () => {
+        const res = await request(app).get('/abcdefg')
+        expect(res.statusCode).toEqual(400);
+        expect(res.text).toContain('Invalid customer id. Must be of type Integer');
+    })
+  })
+
+  describe("GET /add", () => {
+    test("Retrieves a form for adding a new customer when the '/add' endpoint is reached", async () => {
+      const res = await request(app).get('/add/')
+      console.log(res);
+      expect(res.statusCode).toEqual(200);
+      expect(res.text).toContain('Add a Customer');
+      expect(res.text).toContain('First Name:');
+      expect(res.text).toContain('Last Name:');
+      expect(res.text).toContain('Phone:');
+      expect(res.text).toContain('Notes:');
+      expect(res.text).toContain('Add');
+    })
+  })
+
+  describe("POST /:id/edit", () => {
+    test("Sends form information for editing a customer and updates the database when ALL fields are updated", async () => {
+      const res = await request(app).post(`/${testCustomer.id}/edit`).send({
+        firstName: 'Mr. Guy',
+        lastName:  'Man',
+        phone:     '555 444 3333',
+        notes:     'On a diet, just soup and salad now...'
+      })
+      expect(res.statusCode).toEqual(200);
+
+
+
+
+
+    })
+  })
+
+// Look at the videos in section 36 'Express testing practices' for how to send form data when testing...
+// REMEMBER TO FIND A WAY OF SENDING A STATUS CODE OF 201 IN routes.js WHEN YOU TEST 'POST: /add/ '
