c6a64fb5aa8abb4ddf55e34eddfce6da7a05981b
nori <__NORI__@chaos-2.lan>
2021-02-21 12:33:47 -0800

    added some error catching and changed the order of the endpoints, since express apparently takes that into considertion

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const Customer = require("./models/customer");
 const Reservation = require("./models/reservation");
+const ExpressError = require('./expressError');
 
 const router = new express.Router();
 
@@ -9,6 +10,59 @@ router.get("/", async function(req, res, next) {
   try {
     const customers = await Customer.all();
     return res.render("customer_list.html", { customers });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/* Form to add a new customer. */
+router.get("/add/", async function(req, res, next) {
+  try {
+    return res.render("customer_new_form.html");
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/* Show a customer, given their ID. */
+router.get("/:id/", async function(req, res, next) {
+  try {
+    if(isNaN(req.params.id)) throw new ExpressError('Invalid customer id. Must be of type Integer', 400);
+
+    const customer = await Customer.get(req.params.id);
+    const reservations = await customer.getReservations();
+
+    return res.render("customer_detail.html", { customer, reservations });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/*Show form to edit a customer. */
+router.get("/:id/edit/", async function(req, res, next) {
+  try {
+    if(isNaN(req.params.id)) throw new ExpressError('Invalid customer id. Must be of type Integer', 400);
+    
+    const customer = await Customer.get(req.params.id);
+
+    return res.render("customer_edit_form.html", { customer });
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/* Handle editing a customer. */
+router.post("/:id/edit/", async function(req, res, next) {
+  try {
+    if(isNaN(req.params.id)) throw new ExpressError('Invalid customer id. Must be of type Integer', 400);
+    const customer = await Customer.get(req.params.id);
+    customer.firstName = req.body.firstName;
+    customer.lastName = req.body.lastName;
+    customer.phone = req.body.phone;
+    customer.notes = req.body.notes;
+    await customer.save();
+
+    return res.redirect(`/${customer.id}/`);
   } catch (err) {
     return next(err);
   }
@@ -24,14 +78,6 @@ router.post("/search/", async function(req, res, next) {
   }
 });
 
-/* Form to add a new customer. */
-router.get("/add/", async function(req, res, next) {
-  try {
-    return res.render("customer_new_form.html");
-  } catch (err) {
-    return next(err);
-  }
-});
 
 /* Handle adding a new customer. */
 router.post("/add/", async function(req, res, next) {
@@ -50,49 +96,11 @@ router.post("/add/", async function(req, res, next) {
   }
 });
 
-/* Show a customer, given their ID. */
-router.get("/:id/", async function(req, res, next) {
-  try {
-    const customer = await Customer.get(req.params.id);
-
-    const reservations = await customer.getReservations();
-
-    return res.render("customer_detail.html", { customer, reservations });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-/*Show form to edit a customer. */
-router.get("/:id/edit/", async function(req, res, next) {
-  try {
-    const customer = await Customer.get(req.params.id);
-
-    res.render("customer_edit_form.html", { customer });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-/* Handle editing a customer. */
-router.post("/:id/edit/", async function(req, res, next) {
-  try {
-    const customer = await Customer.get(req.params.id);
-    customer.firstName = req.body.firstName;
-    customer.lastName = req.body.lastName;
-    customer.phone = req.body.phone;
-    customer.notes = req.body.notes;
-    await customer.save();
-
-    return res.redirect(`/${customer.id}/`);
-  } catch (err) {
-    return next(err);
-  }
-});
 
 /* Handle adding a new reservation. */
 router.post("/:id/add-reservation/", async function(req, res, next) {
   try {
+    if(isNaN(req.params.id)) throw new ExpressError('Invalid customer id. Must be of type Integer', 400);
     const customerId = req.params.id;
     const startAt = new Date(req.body.startAt);
     const numGuests = req.body.numGuests;
