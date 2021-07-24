619d110cdafc5e4f305a7d62ec85a5b2727808e0
Dylan Feres <dferes23@gmail.com>
2021-07-24 14:15:13 -0700

    allows for deployed url port

--- db.js
+++ db.js
@@ -1,10 +1,25 @@
 const { Client } = require('pg');
 
-let DB_URI = process.env.NODE_ENV === 'test' ? 'postgresql:///lunchly_test' : 'postgresql:///lunchly';
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+    ? "lunchly_test"
+    : process.env.DATABASE_URL || "lunchly";
+}
 
-let db = new Client({
-    connectionString: DB_URI
-});
+if( process.env.NODE_ENV === 'production') {
+  db = new Client({
+    connectionString: getDatabaseUri(),
+    ssl: {
+      rejectUnauthorized: false
+    }  
+  });  
+} else  db = new Client({ connectionString: getDatabaseUri()});
+// let DB_URI = process.env.NODE_ENV === 'test' ? 'postgresql:///lunchly_test' : 'postgresql:///lunchly';
+
+// let db = new Client({
+//     connectionString: DB_URI
+// });
 
 db.connect();
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const PORT = +process.env.PORT || 3000 
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("listening on 3000");
 });
